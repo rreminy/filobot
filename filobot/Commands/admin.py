bb49d4e388c4145168e103fcb8952e3b1d6b282e
Josh H <josh@niveous.co>
2019-05-09 00:44:16 -0400

    Clear command

---
+++ filobot/Commands/admin.py
@@ -0,0 +1,23 @@
+import logging
+import discord
+
+from discord.ext import commands
+
+
+class Admin(commands.Cog):
+    def __init__(self, bot: discord.ext.commands.Bot):
+        self._log = logging.getLogger(__name__)
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(administrator=True)
+    async def clear(self, ctx: commands.context.Context):
+        """
+        Clear recent messages posted by Filo from the channel
+        (Will also clear the message used to instantiate this command)
+        """
+        async for message in ctx.channel.history(limit=50):  # type: discord.Message
+            if message.author.id == self.bot.user.id:
+                await message.delete()
+
+        await ctx.message.delete()

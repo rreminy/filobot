afdb8e9e22b04fc9b38f3b244379b34f163875e7
Josh H <josh@niveous.co>
2019-05-10 02:09:51 -0400

    Random game statuses

--- filobot/__init__.py
+++ filobot/__init__.py
@@ -1,9 +1,13 @@
 import asyncio
 import logging
+import random
 from configparser import ConfigParser
+import discord
+
 from discord.ext import commands
 from filobot.Commands import Hunts
 from filobot.Commands.admin import Admin
+from filobot.Commands.tracker import Tracker
 from filobot.utilities.manager import HuntManager
 
 # Load our configuration
@@ -26,7 +30,11 @@ log.addHandler(ch)
 bot = commands.Bot(command_prefix='f.')
 hunt_manager = HuntManager(bot)
 bot.add_cog(Hunts(bot, hunt_manager))
+bot.add_cog(Tracker(bot, hunt_manager))
 bot.add_cog(Admin(bot))
+
+
+GAMES = ("with moogles", "in Totomo Omo's estate", "in the Izakaya Pub", "pranks on Joel Cleveland", "with the hunt tracker")
 
 
 @bot.event
@@ -48,4 +56,14 @@ async def update_hunts():
             log.exception('Exception thrown while reloading hunts')
         await asyncio.sleep(15.0)
 
+
+async def update_game():
+    await bot.wait_until_ready()
+
+    while not bot.is_closed():
+        game = random.choice(GAMES)
+        await bot.change_presence(activity=discord.Game(name=game))
+        await asyncio.sleep(60.0)
+
 bot.loop.create_task(update_hunts())
+bot.loop.create_task(update_game())

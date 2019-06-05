b758ff2f9821baa64eec046e3091dc10cfbe825d
Totomo Omo <makoto@taiga.sh>
2019-06-05 16:48:42 -0400

    Error handling

--- filobot/Commands/ffxiv.py
+++ filobot/Commands/ffxiv.py
@@ -10,16 +10,13 @@ from filobot.utilities.xivapi import XivApi
 
 class FFXIV(commands.Cog):
 
-    RE_CHAR_STRING = re.compile(r"^(?P<character>[a-zA-Z']+ [a-zA-Z']+)@(?P<world>[a-zA-Z']+)$")
-
     def __init__(self, bot: discord.ext.commands.Bot, api_key: str):
         self._log = logging.getLogger(__name__)
         self.bot = bot
         self.xiv = XivApi(api_key)
 
     @commands.command()
-    @commands.has_permissions(administrator=True)
-    async def iam(self, ctx: commands.context.Context, *, name: str):
+    async def iam(self, ctx: commands.context.Context, world: str, *, character: str):
         """
         Clear recent messages posted by Filo from the channel
         (Will also clear the message used to instantiate this command)
@@ -28,16 +25,15 @@ class FFXIV(commands.Cog):
             await ctx.send("An FFXIV character has already been linked to this Discord account", delete_after=10.0)
             return
 
-        match = self.RE_CHAR_STRING.match(name)
-        if not match:
-            await ctx.send("Invalid character name provided. Please try again and provide a both the character name and world formatted in `Character Name@World` syntax. For example, `Totomo Omo@Mateus`", delete_after=10.0)
+        try:
+            forename, surname = character.lower().split(' ')
+            world             = world.lower()
+        except ValueError:
+            await ctx.send('Invalid character name provided. Please provide the world name first, then your characters first and last names. For example, `f.iam Mateus Totomo Omo`', delete_after=10.0)
             return
 
-        forename, surname = match.group('character').lower().split(' ')
-        world             = match.group('world').lower()
-
         if world.title() not in HuntManager.WORLDS:
-            await ctx.send("Unrecognized world: " + world.title(), delete_after=10.0)
+            await ctx.send('Invalid world provided. Please provide the world name first, then your characters first and last names. For example, `f.iam Mateus Totomo Omo`', delete_after=10.0)
             return
 
         async with ctx.typing():

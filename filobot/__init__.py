7ba9bca49f346b236d13e35a2dbc68535a3c97bf
Totomo Omo <makoto@taiga.sh>
2019-06-07 06:52:18 -0400

    Error printing

--- filobot/__init__.py
+++ filobot/__init__.py
@@ -5,6 +5,7 @@ import os
 import random
 from configparser import ConfigParser
 import discord
+import math
 
 from discord.ext import commands
 from filobot.Commands import Hunts
@@ -52,6 +53,66 @@ async def on_ready():
 
     print('Filo is ready for action!')
     print('------')
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    # if command has local error handler, return
+    if hasattr(ctx.command, 'on_error'):
+        return
+
+    # get the original exception
+    error = getattr(error, 'original', error)
+
+    if isinstance(error, commands.CommandNotFound):
+        return
+
+    if isinstance(error, commands.BotMissingPermissions):
+        missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+        if len(missing) > 2:
+            fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+        else:
+            fmt = ' and '.join(missing)
+        _message = 'I need the **{}** permission(s) to run this command.'.format(fmt)
+        await ctx.send(_message)
+        return
+
+    if isinstance(error, commands.DisabledCommand):
+        await ctx.send('This command has been disabled.')
+        return
+
+    if isinstance(error, commands.CommandOnCooldown):
+        await ctx.send("This command is on cooldown, please retry in {}s.".format(math.ceil(error.retry_after)))
+        return
+
+    if isinstance(error, commands.MissingPermissions):
+        missing = [perm.replace('_', ' ').replace('guild', 'server').title() for perm in error.missing_perms]
+        if len(missing) > 2:
+            fmt = '{}, and {}'.format("**, **".join(missing[:-1]), missing[-1])
+        else:
+            fmt = ' and '.join(missing)
+        _message = 'You need the **{}** permission(s) to use this command.'.format(fmt)
+        await ctx.send(_message)
+        return
+
+    if isinstance(error, commands.UserInputError):
+        await ctx.send("Invalid input.")
+        await self.send_command_help(ctx)
+        return
+
+    if isinstance(error, commands.NoPrivateMessage):
+        try:
+            await ctx.author.send('This command cannot be used in direct messages.')
+        except discord.Forbidden:
+            pass
+        return
+
+    if isinstance(error, commands.CheckFailure):
+        await ctx.send("You do not have permission to use this command.")
+        return
+
+    # ignore all other exception types, but print them to stderr
+    log.exception("An unknown exception occurred while executing a command")
 
 
 # noinspection PyBroadException

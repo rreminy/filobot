e8877ecca2d12cda0f9734bcaf406d0cf2707b79
AzureGem <rreminy@gmail.com>
2020-05-24 23:42:38 -0400

    change lastreported parsing to use fromisoformat

--- filobot/tasks.py
+++ filobot/tasks.py
@@ -75,7 +75,7 @@ async def _process_hunt(source, data):
         x, y    = round((float(data[config.get(source, 'x')]) * 0.02 + _plus)*10)/10, round((float(data[config.get(source, 'y')]) * 0.02 + _plus)*10)/10
         i = data[config.get(source, 'i')] if config.get(source, 'i') in data else 0
         lastreported = data[config.get(source, 'lastReported')]
-        last_seen = time.mktime(time.strptime(lastreported, '%Y-%m-%d %H:%M:%S'))
+        last_seen = datetime.datetime.fromisoformat(lastreported).timestamp()
         xivhunt = {
             'rank': hunt['Rank'],
             'i': i, # data['i'], Seeing as this isn't functional anywhere at the moment
@@ -106,7 +106,7 @@ async def _process_fate(source, data):
         x, y    = round((float(data[config.get(source, 'x')]) * 0.02 + _plus)*10)/10, round((float(data[config.get(source, 'y')]) * 0.02 + _plus)*10)/10
         i = data[config.get(source, 'i')] if config.get(source, 'i') in data else 0
         lastreported = data[config.get(source, 'lastReported')]
-        last_seen = time.mktime(time.strptime(lastreported, '%Y-%m-%d %H:%M:%S'))
+        last_seen = datetime.datetime.fromisoformat(lastreported).timestamp()
         startTimeEpoch = int(data['startTimeEpoch']) if 'startTimeEpoch' in data and data['startTimeEpoch'] and data['startTimeEpoch'].isnumeric() else 0
         duration = int(data['duration']) if 'duration' in data and data['duration'] and data['duration'].isnumeric() else 0
         time_left = (duration - (last_seen - startTimeEpoch)) if duration else -1
